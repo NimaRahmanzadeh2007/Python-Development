a1b1467154c16ed62ee08f8e0e68db8a8067ba2f
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2025-01-15 20:52:46 +0330

    bruh

--- week_1/exercise_2.py
+++ week_1/exercise_2.py
@@ -1,8 +1,9 @@
-number_1 = int(input("Enter the first number: "))
-number_2 = int(input("Enter the seccond number: "))
+while True:
+    number_1 = int(input("Enter the first number: "))
+    number_2 = int(input("Enter the seccond number: "))
 
-print(f"{number_1} + {number_2} = {number_1 + number_2}")
-print(f"{number_1} - {number_2} = {number_1 - number_2}")
-print(f"{number_1} * {number_2} = {number_1 * number_2}")
-print(f"{number_1} / {number_2} = {number_1 / number_2}")
+    print(f"{number_1} + {number_2} = {number_1 + number_2}")
+    print(f"{number_1} - {number_2} = {number_1 - number_2}")
+    print(f"{number_1} * {number_2} = {number_1 * number_2}")
+    print(f"{number_1} / {number_2} = {number_1 / number_2}")
 
--- week_1/exercise_3.py
+++ week_1/exercise_3.py
@@ -1,9 +1,14 @@
-current_year = 1403
+while True:
+    current_year = 1403
 
-user_birth = int(input("Enter your birth year: "))
+    user_birth = int(input("Enter your birth year: "))
+    
+    try:
 
-
-if(current_year < user_birth):
-    print("wrong input!")
-else:
-    print(f"you are {1403 - user_birth} years old")
+        if(current_year < user_birth):
+            print("wrong input!")
+        else:
+            print(f"you are {1403 - user_birth} years old")
+    
+    except:
+        print("Somthing went wrong!")
