a70d4b013cc2c5bcfd57105e53e0bdf6403e8d57
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2025-01-17 01:14:18 +0330

    updated

--- Python/laboratory/star.py
+++
@@ -1,7 +0,0 @@
-for num in range(1,10):
-    if num % 2 == 0:
-        for star in range(1,6):
-            print("*" * star)
-    else:
-        for star in range(6,1,-1):
-            print("*" * star)
