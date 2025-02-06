1bea9aecd0f530d4a52d8fda14b7f0a5ba2890fb
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2025-02-06 20:19:12 +0330

    changing codes and adding readme

---
+++ Python/week_4/README.md
@@ -0,0 +1,13 @@
+# Week 4
+
+## score_calculator.py
+
+In this program, the user is given as many grades as they want to enter, and finally the grade point average is displayed.
+
+## shop.py
+
+In this program, a list with several dictionaries is manually entered and has four operations: adding, finding, deleting, and updating products in a store, which are performed according to the user's input.
+
+## students_search.py
+
+In this program, there is a list with several dictionaries of information about several students with their first name, last name, and class number. The user can find them by searching. If the student is in the list, the information is displayed, otherwise an appropriate error is displayed.
--- Python/week_4/students_search.py
+++ Python/week_4/students_search.py
@@ -9,16 +9,25 @@ students = [
 
 
 def find_student():
+    found = False
     try:
         user_search = input("Enter the name of student: ")
         for i in students:
             if i["name"] == user_search:
+                found = True
                 print("student found!")
                 print("info:")
                 print(f"name: {i['name']}")
                 print(f"family: {i['family']}")
                 print(f"class number: {i['class_number']}")
+                break
+            else:
+                found = False
+        if found == False:
+            print("student not found...")
+
     except:
-        print("student not found...")
+        print("something went wrong, try again...\n")
+
 
 find_student()
