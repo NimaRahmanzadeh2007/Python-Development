96c9eb128561a2b8caf8b8877e2a0106e39e57ea
NimaRahmanzadeh2007 <nimarahmanzadeh2007@gmail.com>
2025-02-06 19:13:41 +0330

    debugging week 4

--- Python/week_4/shop.py
+++ Python/week_4/shop.py
@@ -29,44 +29,102 @@ def add_product():
             product_price = int(
                 input(f"Enter the price of the {product_name}: "))
             products.append(dict(name=product_name, price=product_price))
-            print(f"{product_name} with price: {product_price} added successfully...")
+            print(
+                f"{product_name} with price: {product_price} added successfully...\n")
             break
         except:
-            print("something went wrong, try again...")
+            print("something went wrong, try again...\n")
 
 
 def find_product():
+    found = False
     try:
         user_search = input("Enter the name of the product: ")
         for i in products:
             if i["name"] == user_search:
-                print("product found!")
-                print("info:")
-                print(f"name: {i['name']}\nprice: {i['price']}")
-        print("product not found!")
+                found = True
+                founded_index = products[i]
+                
+
+        if found == False:
+            print("product not found!\n")
+            return
+        else:
+            print("product found!")
+            print("info:")
+            print(f"name: {founded_index['name']}\nprice: {founded_index['price']}\n")
+            return
+
+
     except:
-        print("something went wrong, try again...")
+        print("something went wrong, try again...\n")
 
 
 def remove_product():
+    deleted = False
     try:
         user_search = input("Enter the name of the product: ")
         for i in products:
             if i["name"] == user_search:
                 products.remove(i)
-                print("Product has been deleted successfully.")
-                return
-        print("product not found!")
+                deleted = True
+        if deleted == False:
+            print("product not found!\n")
+        else:
+            print("Product has been deleted successfully.\n")
 
     except:
-        print("something went wrong, try again...")
+        print("something went wrong, try again...\n")
 
 
-# remove_product()
-print(products)
+def update_product():
+    found = False
+    try:
+        user_search = input("Enter the name of the product: ")
+        for i in products:
+            if i["name"] == user_search:
+                found = True
+                print("product found!")
+                print("info:")
+                print(f"name: {i['name']}\nprice: {i['price']}\n")
+                new_name = input("Enter the new name of the product: ")
+                new_price = int(input("Enter the new price of the product: "))
+                i.update({"name": new_name, "price": new_price})
+                print("product updated successfully...\n")
+                return
 
-# add_product()
+            if found == False:
+                print("product not found!\n")
+                return
 
-# print(products)
+    except:
+        print("something went wrong, try again...\n")
 
-# find_product()
+
+while True:
+    try:
+        print("1.add product")
+        print("2.find product")
+        print("3.remove product")
+        print("4.update product")
+        print("-----------------------")
+        user_input = int(input("Choose one(enter the number):"))
+
+        match user_input:
+            case 1:
+
+                add_product()
+            case 2:
+
+                find_product()
+            case 3:
+
+                remove_product()
+            case 4:
+
+                update_product()
+
+            case _:
+                print("invalid input...\n")
+    except:
+        print("something went wrong, try again...\n")
---
+++ Python/week_4/shop_test.py
@@ -0,0 +1,132 @@
+products = [
+    {
+        "name": "shampo",
+        "price": 180,
+    },
+    {
+        "name": "meat",
+        "price": 800,
+    },
+    {
+        "name": "coca-cola",
+        "price": 60,
+    },
+    {
+        "name": "cocoa milk",
+        "price": 15,
+    },
+    {
+        "name": "rangarang",
+        "price": 5,
+    },
+]
+
+
+def add_product():
+    while True:
+        try:
+            product_name = input("Enter the name of the product: ")
+            product_price = int(
+                input(f"Enter the price of the {product_name}: "))
+            products.append(dict(name=product_name, price=product_price))
+            print(
+                f"{product_name} with price: {product_price} added successfully...\n")
+            break
+        except:
+            print("something went wrong, try again...\n")
+
+print(products)
+add_product()
+print(products)
+
+# def find_product():
+#     found = False
+#     try:
+#         user_search = input("Enter the name of the product: ")
+#         for i in products:
+#             if i["name"] == user_search:
+#                 found = True
+#                 founded_index = products[i]
+
+#         if found == False:
+#             print("product not found!\n")
+#             return
+#         else:
+#             print("product found!")
+#             print("info:")
+#             print(
+#                 f"name: {founded_index['name']}\nprice: {founded_index['price']}\n")
+#             return
+
+#     except:
+#         print("something went wrong, try again...\n")
+
+
+# def remove_product():
+#     deleted = False
+#     try:
+#         user_search = input("Enter the name of the product: ")
+#         for i in products:
+#             if i["name"] == user_search:
+#                 products.remove(i)
+#                 deleted = True
+#         if deleted == False:
+#             print("product not found!\n")
+#         else:
+#             print("Product has been deleted successfully.\n")
+
+#     except:
+#         print("something went wrong, try again...\n")
+
+
+# def update_product():
+#     found = False
+#     try:
+#         user_search = input("Enter the name of the product: ")
+#         for i in products:
+#             if i["name"] == user_search:
+#                 found = True
+#                 print("product found!")
+#                 print("info:")
+#                 print(f"name: {i['name']}\nprice: {i['price']}\n")
+#                 new_name = input("Enter the new name of the product: ")
+#                 new_price = int(input("Enter the new price of the product: "))
+#                 i.update({"name": new_name, "price": new_price})
+#                 print("product updated successfully...\n")
+#                 return
+
+#             if found == False:
+#                 print("product not found!\n")
+#                 return
+
+#     except:
+#         print("something went wrong, try again...\n")
+
+
+# while True:
+#     try:
+#         print("1.add product")
+#         print("2.find product")
+#         print("3.remove product")
+#         print("4.update product")
+#         print("-----------------------")
+#         user_input = int(input("Choose one(enter the number):"))
+
+#         match user_input:
+#             case 1:
+
+#                 add_product()
+#             case 2:
+
+#                 find_product()
+#             case 3:
+
+#                 remove_product()
+#             case 4:
+
+#                 update_product()
+
+#             case _:
+#                 print("invalid input...\n")
+#     except:
+#         print("something went wrong, try again...\n")
